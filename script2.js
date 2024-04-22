cbe784d1781d51deb8e632a3faba0da4bd2459ae
Filipe <98906391+Filipe2508@users.noreply.github.com>
2024-04-22 02:22:00 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,22 @@
+let timer = null;
+
+function startScroll() {
+  const input = document.getElementById('message');
+  if (timer === null) {
+    timer = setInterval(() => {
+      let text = input.value;
+      text = text.substring(1) + text.substring(0, 1);
+      input.value = text;
+    }, 200); 
+  }
+}
+
+function stopScroll() {
+  clearInterval(timer);
+  timer = null;
+}
+
+window.onload = () => {
+  document.getElementById('startButton').addEventListener('click', startScroll);
+  document.getElementById('stopButton').addEventListener('click', stopScroll);
+};
